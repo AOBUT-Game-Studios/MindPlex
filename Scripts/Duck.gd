1772cee343a7a18db48e20fa48982474cc7d4592
Marcus Kuffner <marcuskuffner3125@gmail.com>
2021-04-29 18:16:00 -0400

    Began new duck minigame

---
+++ Scripts/Duck.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+export (float) var h_speed : float = 100
+export (float) var fall_speed : float = 10
+export (float) var jump_height : float = 20
+var gravity = 0
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("West"):
+		velocity.x += -1 * h_speed
+	if Input.is_action_pressed("East"):
+		velocity.x += 1 * h_speed
+	if Input.is_action_just_pressed("Spacebar"):
+		gravity = jump_height
+	gravity -= fall_speed
+	velocity.y -= gravity
+
+		
+func _physics_process(delta):
+	get_input()
+	move_and_slide(velocity)
