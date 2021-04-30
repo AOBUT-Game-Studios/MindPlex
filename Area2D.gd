96a0ad5ad8ebe7bc9514f482245682561aa7b322
unknown <Qazeymoto>
2021-04-30 19:33:12 -0400

    amogus game complete needs visuals

--- Area2D.gd
+++ Area2D.gd
@@ -1,16 +1,31 @@
 extends Area2D
-export(int) var sus_detect = 1
-var rng = RandomNumberGenerator.new()
+export(int) var sus_detect = -1
+#var rng = RandomNumberGenerator.new()
 var amog_position
+var amog_slot
 
-func randompos():
-	rng.randomize()
-	amog_position = Vector2(rng.randi_range(0, 750), rng.randi_range(0, 500))
-	self.global_position = amog_position
-func _ready():
-	randompos()
+func amogpos(slot):
+	if slot == 0:
+		self.global_position = Vector2(200, 100)
+	elif slot == 1:
+		self.global_position = Vector2(500, 100)
+	elif slot == 2:
+		self.global_position = Vector2(800, 100)
+	elif slot == 3:
+		self.global_position = Vector2(200, 250)
+	elif slot == 4:
+		self.global_position = Vector2(500, 250)
+	elif slot == 5:
+		self.global_position = Vector2(800, 250)
+	elif slot == 6:
+		self.global_position = Vector2(200, 400)
+	elif slot == 7:
+		self.global_position = Vector2(500, 400)
+	elif slot == 8:
+		self.global_position = Vector2(800, 400)
+
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			get_parent().sus_time(sus_detect)
-			#queue_free()
+			self.visible = false
+			get_parent().sus_check(sus_detect)
