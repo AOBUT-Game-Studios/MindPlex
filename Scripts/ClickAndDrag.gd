b728cf987022c314c8537e9415c9d1d11ff74e52
Marcus Kuffner <marcuskuffner3125@gmail.com>
2021-05-01 18:32:30 -0400

    fixed bread

--- Scripts/ClickAndDrag.gd
+++ Scripts/ClickAndDrag.gd
@@ -10,14 +10,6 @@ func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			emit_signal("clicked", self)
-			
-func _integrate_forces(state):
-	if held:
-		add_central_force(get_local_mouse_position()/5)
-		#add_torque(get_local_mouse_position().x)
-		#add_torque(get_local_mouse_position().y)
-	else:
-		add_central_force(-applied_force/50)
 	
 	
 func pickup():
@@ -25,6 +17,8 @@ func pickup():
 		return
 	#mode = RigidBody2D.MODE_STATIC
 	held = true
+	set_collision_mask_bit(1, true)
+	gravity_scale = 2
 	#originpos = global_transform.origin
 	#global_transform.origin = get_global_mouse_position()
 	#get_node("CollisionShape2D").global_transform.origin  = originpos
@@ -34,6 +28,8 @@ func drop(impulse=Vector2.ZERO):
 		#mode = RigidBody2D.MODE_RIGID
 		#apply_central_impulse(impulse)
 		held = false
+		set_collision_mask_bit(1, false)
+		gravity_scale = 5
 		#global_transform.origin = get_node("CollisionShape2D").transform.origin
 		#get_node("CollisionShape2D").position = Vector2()
 
